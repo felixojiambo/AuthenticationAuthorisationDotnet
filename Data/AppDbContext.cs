319b0ba94a465139734fb5b41de832ae0a04a244
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-13 06:35:14 -0700

    setup project

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using AuthenticationAuthorisation.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationAuthorisation.Data
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+            
+        }
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationAuthorisation.Models
+{
+    public class AppUser:IdentityUser
+    {
+    public String FullName{get;set;}    
+    }
+}
